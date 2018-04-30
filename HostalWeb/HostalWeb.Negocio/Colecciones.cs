9d977335d6333085ae5c42402675ab6f9e9f5be3
HugoAraya <38479140+hugoaraya@users.noreply.github.com>
2018-04-30 04:26:37 -0300

    Carga de Base de Datos Nueva Estructura con Script 4.1
    --- Se cambio al nuevo modelo pero genero errores en Visual Studio al cargar ---
    
    Por esto modifique script para cargar las sigientes tablas.
    USUARIO
    TIPO_USUARIO
    ESTADO_EMPLEADO
    EMPLEADO
    EMPRESA
    TRABAJADOR
    RUBRO
    PROVEEDOR
    
    - DEJO SCript con modificaciones...
    
    Aplicacion funcionando...

--- HostalWeb/HostalWeb.Negocio/Colecciones.cs
+++ HostalWeb/HostalWeb.Negocio/Colecciones.cs
@@ -18,11 +18,11 @@ namespace HostalWeb.Negocio
                 salida.Add(
                     new Empresa()
                     {
-                        ID_EMPRESA = datosempresa.ID_EMPRESA,
-                        RUT_EMPRESA = datosempresa.RUT_EMPRESA + datosempresa.DV_EMPRESA,
-                        NOMBRE_EMPRESA = datosempresa.NOMBRE_EMPRESA,
-                        DIRECCION_EMPRESA = datosempresa.DIRECCION_EMPRESA,
-                        GIRO_EMPRESA = datosempresa.GIRO_EMPRESA
+                        ID_EMPRESA = datosempresa.ID,
+                        RUT_EMPRESA = datosempresa.RUT + datosempresa.DV,
+                        NOMBRE_EMPRESA = datosempresa.NOMBRE,
+                        DIRECCION_EMPRESA = datosempresa.DIRECCION,
+                        GIRO_EMPRESA = datosempresa.GIRO
                     }
           );
             }
